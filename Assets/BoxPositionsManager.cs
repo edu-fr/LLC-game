e6dbdbf3bb1e95ddf7d00bbb2c47cb8dc1e2a5cf
Eduardo F Ribeiro <ra99571@uem.br>
2022-01-24 18:12:53 -0300

    box-position-anchors-created

---
+++ Assets/BoxPositionsManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxPositionsManager : MonoBehaviour
+{
+    // ANCHORS
+    // Phase 1 Part 1
+    public Transform Anchor_Phase1Part1_CLF_Box_gray;
+    public Transform Anchor_Phase1Part1_variablesBox;
+    public Transform Anchor_Phase1Part1_usefulVariablesBox;
+    public Transform Anchor_Phase1Part1_uselessVariablesBox;
+    
+    // Phase 2 Part 2
+    public Transform Anchor_Phase1Part2_CLF_Box;
+    public Transform Anchor_Phase1Part2_usefulVariablesBox_gray;
+    public Transform Anchor_Phase1Part2_uselessVariablesBox_gray;
+    
+}
--- Assets/Resources/Scripts/LevelScript.cs
+++ Assets/Resources/Scripts/LevelScript.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 namespace Resources.Scripts
 {
@@ -14,15 +15,20 @@ namespace Resources.Scripts
         private List<char> _lambdaProducers = new List<char>();
 
         // ReSharper disable once InconsistentNaming
-        [SerializeField] private GameObject _CFL_Box;
+        [SerializeField] private GameObject CFL_Box;
         [SerializeField] private GameObject variablesBox;
         [SerializeField] private GameObject usefulVariablesBox;
         [SerializeField] private GameObject uselessVariablesBox;
-        private int currentPhase;
-        private int currentPart;
+        private int _currentPhase;
+        private int _currentPart;
+        
+        // Box positions
+        private BoxPositionsManager _boxPositionsManager;
+
         private void Awake()
         {
-            
+            _boxPositionsManager = GetComponent<BoxPositionsManager>();
+           
         }
         
         private void Start()
@@ -31,29 +37,36 @@ namespace Resources.Scripts
             _grammar = grammarObject.GetComponent<GrammarScript>();
             // Analyse productions
             _grammar.Setup();
-            _CFL_Box.GetComponent<ProductionsBox>().FillWithProductions(_grammar.Productions.ToList());
-            _CFL_Box.GetComponent<ProductionsBox>().SetGrayScale(true);
-            variablesBox.GetComponent<VariablesBox>().FillWithVariables(_grammar.Variables);
-
-            currentPhase = 1;
-            currentPart = 1;
-
+            
+            // Set initial phase and part
+            _currentPhase = 1;
+            _currentPart = 1;
+            
+            SetupPhase1Part1();
         }
 
         public void TryNextPhase()
         {
-            switch (currentPhase)
+            switch (_currentPhase)
             {
                 case 1:
 
-                    switch (currentPart)
+                    switch (_currentPart)
                     {
                         case 1:
-                            Phase1Part1();
+                            if (Phase1Part1())
+                            {
+                                _currentPart = 2; 
+                                SetupPhase1Part2();
+                            };
                             break;
                         
                         case 2:
-
+                            if (Phase1Part2())
+                            {
+                                // currentPart = 3;
+                                // SetupPhase1Part3();
+                            }
                             break;
                     }
                     
@@ -66,13 +79,13 @@ namespace Resources.Scripts
             }
         }
 
-        private void Phase1Part1()
+        private bool Phase1Part1()
         {
             var currentVariablesOnVariablesBox = variablesBox.GetComponent<VariablesBox>().variableList;
             if (currentVariablesOnVariablesBox.Count > 0)
             {
                 print("Ainda há variáveis que devem ser movidas!");
-                return;
+                return false;
             }
             
             var currentVariablesOnUsefulBox = usefulVariablesBox.GetComponent<VariablesBox>().variableList;
@@ -87,11 +100,55 @@ namespace Resources.Scripts
                     if (!correctVariables.Contains(variable))
                     {
                         print("A variavel " + variable + " não faz parte da lista de variáveis corretas!");
-                        return;
+                        return false;
                     } 
                 }
                 print("Correto! Pode prosseguir para a próxima fase!");
+                return true;
             }
+            return false;
+        }
+
+        
+        private void SetupPhase1Part1()
+        {
+            print("Setting up phase 1 part 1");
+            // Filling boxes
+            CFL_Box.GetComponent<ProductionsBox>().FillWithProductions(_grammar.Productions.ToList());
+            variablesBox.GetComponent<VariablesBox>().FillWithVariables(_grammar.Variables);
+            
+            // Changing productions box color
+            CFL_Box.GetComponent<ProductionsBox>().SetGrayScale(true);
+
+            // Setting positions
+            CFL_Box.transform.position = _boxPositionsManager.Anchor_Phase1Part1_CLF_Box_gray.position;
+            variablesBox.transform.position = _boxPositionsManager.Anchor_Phase1Part1_variablesBox.position;
+            uselessVariablesBox.transform.position = _boxPositionsManager.Anchor_Phase1Part1_uselessVariablesBox.position;
+            usefulVariablesBox.transform.position = _boxPositionsManager.Anchor_Phase1Part1_usefulVariablesBox.position;
+        }
+        
+        private void SetupPhase1Part2()
+        {
+            print("Setting up phase 1 part 2");
+            
+            // Deactivating unused boxes
+            variablesBox.SetActive(false);
+            usefulVariablesBox.SetActive(false);
+            
+            // Setting positions
+            CFL_Box.transform.position = _boxPositionsManager.Anchor_Phase1Part2_CLF_Box.position;
+            uselessVariablesBox.transform.position =
+                _boxPositionsManager.Anchor_Phase1Part2_uselessVariablesBox_gray.position;
+
+                // Changing box colors
+            CFL_Box.GetComponent<ProductionsBox>().SetGrayScale(false);
+            uselessVariablesBox.GetComponent<VariablesBox>().SetGrayScale(true);
+            
+        }
+
+        private bool Phase1Part2()
+        {
+            return false;
         }
         private void Update()
         {
